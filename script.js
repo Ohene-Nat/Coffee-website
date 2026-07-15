6770b9f514858ca713d59061ad0e6a0ce44b3232
Nathaniel <186203419+Ohene-Nat@users.noreply.github.com>
2026-07-15 23:19:08 +0000

    added a testimonial section
    added a testimonial section with a swiper effect

--- script.js
+++ script.js
@@ -8,3 +8,34 @@ menuCloseButton.addEventListener('click', () => {
     //toggle the class 'show-mobile-menu' on the body element when the menu button is clicked
     document.body.classList.toggle("show-mobile-menu");
 });
+
+// Initialize Swiper
+const swiper = new Swiper('.swiper', {
+
+  loop: true,
+    spaceBetween: 25,
+    grabCursor: true,
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,    
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    }
+  }
+});
